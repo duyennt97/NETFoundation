226dec3b7798fda9aa2ee36b6685d7121c978253
Thien Duyen Nguyen <dth@elca.vn>
2021-09-19 14:29:36 +0700

    Update AutoMapper

--- BookStoreBusiness/Mapper/AutoMapper.cs
+++ BookStoreBusiness/Mapper/AutoMapper.cs
@@ -36,45 +36,5 @@ namespace BookStoreConsole.Mapper
 
             return PriceCategory.Normal;
         }
-
-        private MapperConfiguration CreateConfiguration()
-        {
-            var config = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile<MappingProfile>();   
-            });
-
-            return config;
-        }
-    }
-
-    public class MappingProfile : Profile
-    {
-        public MappingProfile()
-        {
-            CreateMap<Book, BookEntity>()
-                .ForMember(dest => dest.PublishYear,
-                    opt => opt.MapFrom(src => Convert.ToInt32(src.PublishYear)))
-                .ForMember(dest => dest.Price,
-                    opt => opt.MapFrom(src => Convert.ToInt32(src.Price)))
-                .ForMember(dest => dest.BookCategory,
-                    opt => opt.MapFrom(src => GetBookCategory(src.Price))).ReverseMap();
-        }
-
-        private PriceCategory GetBookCategory(string srcPrice)
-        {
-            var price = Convert.ToInt32(srcPrice);
-            if (price <= 100)
-            {
-                return PriceCategory.Cheap;
-            }
-
-            if (price >= 1000)
-            {
-                return PriceCategory.Expensive;
-            }
-
-            return PriceCategory.Normal;
-        }
     }
 }
